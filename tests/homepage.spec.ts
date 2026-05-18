57466a610ec4eecf34af892d58e1aabc1b66993b
Sofia Cassarino <sofia.cassarino.it@gmail.com>
2026-05-18 17:18:55 +0200

    complete homepage test cases

--- tests/homepage.spec.ts
+++ tests/homepage.spec.ts
@@ -1,7 +1,10 @@
 import { expect, test } from "@playwright/test";
 
-test('homepage loads with key elements', async ({ page }) => {
+test.beforeEach(async ({ page }) => {
   await page.goto('https://practicesoftwaretesting.com/');
+});
+
+test('homepage loads with key elements', async ({ page }) => {
   // check page tab title
   await expect(page).toHaveTitle(/Practice Software Testing - Toolshop/);
   // check logo
@@ -13,13 +16,10 @@ test('homepage loads with key elements', async ({ page }) => {
 });
 
 test('at least 9 products are displayed', async ({ page }) => {
-  await page.goto('https://practicesoftwaretesting.com/');
   await expect(page.locator('.card')).toHaveCount(9);
 });
 
 test('search bar is visible and functional', async ({ page }) => {
-    // await for the page to go to 
-    await page.goto('https://practicesoftwaretesting.com/');
     // check search input is visible
     await expect(page.locator('[data-test="search-query"]')).toBeVisible();
     // type in search box
@@ -28,4 +28,49 @@ test('search bar is visible and functional', async ({ page }) => {
     await page.locator('[data-test="search-submit"]').click();
     // check results appear
     await expect(page.locator('[data-test="search-caption"]')).toContainText('Searched for: Hammer');
+});
+
+test('nav menu redirects to the correct pages', async ({page}) => {
+    // click on home button
+    await page.locator('[data-test="nav-home"]').click();
+    await expect(page).toHaveURL('https://practicesoftwaretesting.com/');
+    // click on contact button 
+    await page.locator('[data-test="nav-contact"]').click();
+    await expect(page).toHaveURL('https://practicesoftwaretesting.com/contact');
+    await expect(page).toHaveTitle(/Contact Us - Practice Software Testing/);
+    // click on sign up button
+    await page.locator('[data-test="nav-sign-in"]').click();
+    await expect(page).toHaveURL('https://practicesoftwaretesting.com/auth/login');
+    // click on categories dropdown menu
+    await page.locator('[data-test="nav-categories"]').click();
+    await expect(page.locator('[data-test="nav-hand-tools"]')).toBeVisible();
+    await page.locator('[data-test="nav-hand-tools"]').click();
+    await expect(page).toHaveURL('https://practicesoftwaretesting.com/category/hand-tools');
+    // click on language button
+    await page.locator('[data-test="language-select"]').click();
+    await expect(page.locator('[data-test="lang-de"]')).toBeVisible();
+});
+
+[
+  { name: 'EN', expected: 'Home' },
+  { name: 'DE', expected: 'Start' },
+  { name: 'ES', expected: 'Inicio' },
+  { name: 'FR', expected: 'Accueil' },
+  { name: 'NL', expected: 'Home' },
+  { name: 'TR', expected: 'Anasayfa' },
+].forEach(({ name, expected }) => {
+  test(`switch language translate the whole page into the selected lang ${name}`, async ({ page }) => {
+    await page.locator('[data-test="language-select"]').click();
+    await page.locator(`[data-test="lang-${name.toLowerCase()}"]`).click();
+    await expect(page.locator('[data-test="nav-home"]')).toHaveText(expected);
+  });
+});
+
+test('categories dropdown shows subcategories', async ({ page }) => {
+    await page.locator('[data-test="nav-categories"]').click();
+    await expect(page.locator('[data-test="nav-hand-tools"]')).toBeVisible();
+    await expect(page.locator('[data-test="nav-power-tools"]')).toBeVisible();
+    await expect(page.locator('[data-test="nav-other"]')).toBeVisible();
+    await expect(page.locator('[data-test="nav-special-tools"]')).toBeVisible();
+    await expect(page.locator('[data-test="nav-rentals"]')).toBeVisible(); 
 });
