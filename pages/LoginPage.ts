90b02f7efd68b42e5e55e1f701495f60471ebb10
Sofia Cassarino <sofia.cassarino.it@gmail.com>
2026-07-13 22:18:00 +0200

    added login

--- pages/LoginPage.ts
+++ pages/LoginPage.ts
@@ -1,4 +1,4 @@
-import { Page } from '@playwright/test';
+import { Locator, Page } from '@playwright/test';
 import { BasePage } from './BasePage';
 export class LoginPage extends BasePage {
     constructor(page: Page) {
@@ -6,11 +6,27 @@ export class LoginPage extends BasePage {
     }
 
     async goto() {
-        await this.page.goto('https://practicesoftwaretesting.com/auth/login');
+        await this.page.goto('/auth/login');
+    }
+
+    get emailInput(): Locator {
+        return this.page.locator('data-test="email"');
+    }
+
+    get passwordInput(): Locator {
+        return this.page.locator('data-test="password"');
+    }
+
+    get loginSubmitButton(): Locator {
+        return this.page.locator('data-test="login-submit"');
+    }
+
+    get loginError(): Locator {
+        return this.page.locator('data-test="login-error"');
     }
     async login(email: string, password: string) {
-    await this.page.fill('[data-test="email"]', email);
-    await this.page.fill('[data-test="password"]', password);
-    await this.page.locator('[data-test="login-submit"]').click();
+    await this.emailInput.fill(email);
+    await this.passwordInput.fill(password);
+    await this.loginSubmitButton.click();
     }
 }
--- tests/UI/login.spec.ts
+++ tests/UI/login.spec.ts
@@ -1,16 +1,18 @@
-import { expect, test } from "@playwright/test";
-import { LoginPage } from '../../pages/LoginPage';
+import { test, expect } from '../../fixtures/pages.fixture';
 
-test('login with valid credentials', async ({page}) => {
-    const loginPage = new LoginPage(page);
-    await loginPage.goto();
-    await loginPage.login(process.env.TEST_USER_EMAIL!, process.env.TEST_USER_PASSWORD!);    
-    await expect(page).toHaveURL(/account/);
+test.beforeEach(async ({ loginPage }) => {
+  await loginPage.goto();
 });
 
-test('login with invalid credentials shows error', async ({ page }) => {
-  const loginPage = new LoginPage(page);
-  await loginPage.goto();
+test('login with valid credentials', async ({ page, loginPage }) => {
+  await loginPage.login(
+    process.env.TEST_USER_EMAIL!,
+    process.env.TEST_USER_PASSWORD!
+  );
+  await expect(page).toHaveURL(/account/);
+});
+
+test('login with invalid credentials shows error', async ({ loginPage }) => {
   await loginPage.login('wrong@email.com', 'wrongpassword');
-  await expect(page.locator('[data-test="login-error"]')).toBeVisible();
+  await expect(loginPage.loginError).toBeVisible();
 });
